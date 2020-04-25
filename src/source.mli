8653a1457fb9974e7154e8f8ba872fd8298d736d
Romain Beauxis <toots@rastageeks.org>
2020-04-24 19:35:15 -0500

    Avoid copying video content unless told to.

--- src/source.mli
+++ src/source.mli
@@ -156,6 +156,11 @@ class virtual source :
        (** [is_ready] tells you if [get] can be called. *)
        method virtual is_ready : bool
 
+       (** True if the source manipulates video data in place. If so,
+           shared video content is copied between being passed to this
+           operator. *)
+       method needs_fresh_video : bool
+
        (** [get buf] asks the source to fill the buffer [buf] if possible.
            The [get] call is partial when the buffer is not completely filled.
            [get] should never be called with a full buffer,
--- src/stream/frame.ml
+++ src/stream/frame.ml
@@ -422,7 +422,7 @@ let set_all_metadata b l = b.metadata <- l
 let get_past_metadata b =
   try Some (List.assoc (-1) b.metadata) with Not_found -> None
 
-let blit_content src src_pos dst dst_pos len =
+let blit_content ~copy_video src src_pos dst dst_pos len =
   Array.iter2
     (fun a a' ->
       if a != a' then (
@@ -431,11 +431,20 @@ let blit_content src src_pos dst dst_pos len =
           (Audio.Mono.sub a !src_pos !len)
           (Audio.Mono.sub a' !dst_pos !len) ))
     src.audio dst.audio;
+
+  let blit_video =
+    if copy_video then Video.blit
+    else fun src src_pos dst dst_pos len ->
+      for i = 0 to len - 1 do
+        dst.(dst_pos + 1) <- src.(src_pos + i)
+      done
+  in
+
   Array.iter2
     (fun v v' ->
       if v != v' then (
         let ( ! ) = video_of_master in
-        Video.blit v !src_pos v' !dst_pos !len ))
+        blit_video v !src_pos v' !dst_pos !len ))
     src.video dst.video;
   Array.iter2
     (fun m m' ->
@@ -445,11 +454,12 @@ let blit_content src src_pos dst dst_pos len =
     src.midi dst.midi
 
 (** Copy data from [src] to [dst].
-  * This triggers changes of contents layout if needed. *)
-let blit src src_pos dst dst_pos len =
+    Video frames are assigned unless a copy is
+    requested. *)
+let blit ~copy_video src src_pos dst dst_pos len =
   (* Assuming that the tracks have the same track layout,
    * copy a chunk of data from [src] to [dst]. *)
-  blit_content src.content src_pos dst.content dst_pos len
+  blit_content ~copy_video src.content src_pos dst.content dst_pos len
 
 (** Raised by [get_chunk] when no chunk is available. *)
 exception No_chunk
@@ -458,12 +468,12 @@ exception No_chunk
   * (a chunk is a region of a frame between two breaks).
   * Metadata relevant to the copied chunk is copied as well,
   * and content layout is changed if needed. *)
-let get_chunk ab from =
+let get_chunk ~copy_video ab from =
   assert (is_partial ab);
   let p = position ab in
   let copy_chunk i =
     add_break ab i;
-    blit from p ab p (i - p);
+    blit ~copy_video from p ab p (i - p);
 
     (* If the last metadata before [p] differ in [from] and [ab],
      * copy the one from [from] to [p] in [ab].
